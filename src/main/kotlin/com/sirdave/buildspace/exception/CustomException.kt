c398c1209815ca3874005b00dc0a34885080fa27
Abiola David <davidolanrewaju2000@gmail.com>
2023-05-16 17:23:27 +0100

    Implemented exception handling

---
+++ src/main/kotlin/com/sirdave/buildspace/exception/CustomException.kt
@@ -0,0 +1,9 @@
+package com.sirdave.buildspace.exception
+
+class EntityExistsException(message: String) : Exception(message)
+
+class EntityNotFoundException(message: String) : Exception(message)
+
+class PasswordsDoNotMatchException(message: String) : Exception(message)
+
+
