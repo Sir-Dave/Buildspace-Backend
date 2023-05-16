4f6a93096b6dcdfadd8902c2d9586918adb4503b
Abiola David <davidolanrewaju2000@gmail.com>
2023-05-16 13:36:57 +0100

    Created file for security constants

---
+++ src/main/kotlin/com/sirdave/buildspace/constants/SecurityConstants.kt
@@ -0,0 +1,16 @@
+package com.sirdave.buildspace.constants
+
+object SecurityConstants {
+    const val EXPIRATION_DATE = 602_000_000 // 7 days
+    const val TOKEN_PREFIX = "Bearer "
+    const val JWT_TOKEN_HEADER = "Jwt-Token"
+    const val JWT_ISSUER = "BuildSpace Company"
+    const val JWT_AUDIENCE = "BuildSpace Backend Application"
+    const val AUTHORITIES = "authorities"
+    const val OPTIONS_HTTP_METHOD = "OPTIONS"
+    const val TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified"
+    const val FORBIDDEN_MESSAGE = "You need to log in to access this page"
+    const val ACCESS_DENIED = "You do not have permission to access this page"
+    val PUBLIC_URLS = arrayOf("/api/v1/auth/login", "/api/v1/auth/register",
+        "/api/v1/auth/reset-password/**")
+}
