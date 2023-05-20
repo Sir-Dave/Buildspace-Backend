91c5918d884c450678cb5230d34dc7bc71f59b8c
Abiola David <davidolanrewaju2000@gmail.com>
2023-05-20 04:23:58 +0100

    Implemented method to create verification token

--- src/main/kotlin/com/sirdave/buildspace/token/TokenService.kt
+++ src/main/kotlin/com/sirdave/buildspace/token/TokenService.kt
@@ -1,10 +1,12 @@
 package com.sirdave.buildspace.token
 
+import com.sirdave.buildspace.user.User
+
 interface TokenService {
 
     fun findByToken(token: String): Token
 
     fun confirmToken(token: String): Token
 
-    fun saveToken(token: Token)
+    fun createVerificationToken(user: User): Token
 }
--- src/main/kotlin/com/sirdave/buildspace/token/TokenServiceImpl.kt
+++ src/main/kotlin/com/sirdave/buildspace/token/TokenServiceImpl.kt
@@ -1,8 +1,10 @@
 package com.sirdave.buildspace.token
 
 import com.sirdave.buildspace.exception.EntityNotFoundException
+import com.sirdave.buildspace.user.User
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
+import java.util.*
 import javax.transaction.Transactional
 
 @Service
@@ -24,13 +26,24 @@ class TokenServiceImpl(private val repository: TokenRepository) : TokenService {
             "Token expired"
         }
 
-        if (confirmationToken.confirmedAt == null) //only confirm the token if it has not already
+        //only confirm the token if it has not already
+        if (confirmationToken.confirmedAt == null){
             confirmationToken.confirmedAt = LocalDateTime.now()
 
+            //Activate the user after confirming the token
+            confirmationToken.user.isActive = true
+        }
         return confirmationToken
     }
 
-    override fun saveToken(token: Token){
-        repository.save<Token>(token)
+    override fun createVerificationToken(user: User): Token {
+        val token = UUID.randomUUID().toString()
+        val confirmationToken = Token(
+            token = token,
+            createdAt = LocalDateTime.now(),
+            expiresAt = LocalDateTime.now().plusHours(24),
+            user = user
+        )
+        return repository.save(confirmationToken)
     }
 }
