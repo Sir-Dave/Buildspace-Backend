86be283e3468a3e44e4c9617d697428b9ba24a5e
Abiola David <davidolanrewaju2000@gmail.com>
2023-05-20 02:50:48 +0100

    Created token entity

---
+++ src/main/kotlin/com/sirdave/buildspace/token/Token.kt
@@ -0,0 +1,30 @@
+package com.sirdave.buildspace.token
+
+import com.sirdave.buildspace.user.User
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "tokens")
+class Token(
+    @Column(nullable = false)
+    val token: String,
+
+    @Column(nullable = false)
+    val createdAt: LocalDateTime,
+
+    @Column(nullable = false)
+    val expiresAt: LocalDateTime,
+
+    var confirmedAt: LocalDateTime?,
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    val user: User,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "token_sequence")
+    @SequenceGenerator(name = "token_sequence", sequenceName = "token_sequence", allocationSize = 1)
+    @Column(nullable = false, updatable = false)
+    val id: Long? = null
+)
