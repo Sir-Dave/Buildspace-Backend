6d42aabb3f31ab8236b6e4eb372b4770803fc4aa
Abiola David <davidolanrewaju2000@gmail.com>
2023-05-22 14:21:36 +0100

    Set SMTP username and password as env variables

--- src/main/kotlin/com/sirdave/buildspace/listeners/AuthListener.kt
+++ src/main/kotlin/com/sirdave/buildspace/listeners/AuthListener.kt
@@ -3,25 +3,35 @@ package com.sirdave.buildspace.listeners
 import com.sirdave.buildspace.email.EmailService
 import com.sirdave.buildspace.event.AuthEvent
 import com.sirdave.buildspace.token.TokenService
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 
 @Component
 class AuthListener(
     private val tokenService: TokenService,
-    private val emailService: EmailService
-) {
+    private val emailService: EmailService) {
+
+    @EventListener
     fun onRegistrationSuccess(event: AuthEvent){
-        val emailSubject = "Registration Confirmation"
+        val emailSubject = "Welcome To BuildSpace"
 
         val user = event.user
         val recipientAddress = user.email
 
         val token = tokenService.createVerificationToken(user)
-        val confirmationUrl = "${event.appUrl}/registrationConfirm?token=${token.token}"
+        val confirmationUrl = "${event.appUrl}/confirm?token=${token.token}"
 
-        // TODO: Put the appropriate body later on
-        val body = ""
+        println("confirmation url is $confirmationUrl")
+
+        val body = """Hello ${user.firstName},
+Welcome to BuildSpace. Please click here $confirmationUrl to verify your account. 
+Please note that the verification link is only valid for 24 hours.
+
+
+Have a nice day!
+The Support Team,
+BuildCodeTogether Limited"""
 
         emailService.sendEmailToUser(
             recipientAddress,
