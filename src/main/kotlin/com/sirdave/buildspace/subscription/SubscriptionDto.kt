b35fabfd52c631e8a229e6aee77c58dca7440237
Abiola David <davidolanrewaju2000@gmail.com>
2023-05-23 14:39:31 +0100

    Created dto class for subscription request

--- src/main/kotlin/com/sirdave/buildspace/subscription/SubscriptionDto.kt
+++ src/main/kotlin/com/sirdave/buildspace/subscription/SubscriptionDto.kt
@@ -2,6 +2,12 @@ package com.sirdave.buildspace.subscription
 
 import com.sirdave.buildspace.user.UserDto
 
+
+data class SubscriptionRequest (
+    var userId: String,
+    val type: String
+)
+
 data class SubscriptionDto (
     val id: String,
     val startDate: String,
