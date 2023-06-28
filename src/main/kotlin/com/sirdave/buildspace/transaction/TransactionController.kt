70bfb045b05451b9daef4ea8272c1e5914e725fa
Abiola David <davidolanrewaju2000@gmail.com>
2023-06-28 16:28:42 +0100

    Exposed transactions endpoint

---
+++ src/main/kotlin/com/sirdave/buildspace/transaction/TransactionController.kt
@@ -0,0 +1,26 @@
+package com.sirdave.buildspace.transaction
+
+import com.sirdave.buildspace.mapper.toTransactionDto
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(path = ["/api/v1/transactions"])
+class TransactionController(private val transactionService: TransactionService) {
+
+    @Operation(summary = "Get all transactions by a user")
+    @GetMapping
+    fun getAllSubscriptionsByUser(@RequestParam email: String): ResponseEntity<Set<TransactionDto>>{
+        val transactions = transactionService.getUserTransactions(email)
+        return ResponseEntity(transactions, HttpStatus.OK)
+    }
+
+    @Operation(summary = "Get details of one transaction")
+    @GetMapping("/{id}")
+    fun getSubscriptionById(@PathVariable("id") id: Long): ResponseEntity<TransactionDto>{
+        val transaction = transactionService.findTransactionById(id).toTransactionDto()
+        return ResponseEntity(transaction, HttpStatus.OK)
+    }
+}
